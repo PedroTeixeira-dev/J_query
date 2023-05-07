96d9f1dcc093205333adcfe2ebdc36e019136136
PedroTeixeira-dev <123015308+PedroTeixeira-dev@users.noreply.github.com>
2023-05-07 13:44:11 -0300

    Exercicio de plugins do jquery

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+    $('#cpf').mask('000.000.000-00')
+
+    $('#cep').mask('00000-000')
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('#formulario').submit(function(event){
+        event.preventDefault()
+
+        alert('cadastro realizado')
+
+        this.reset()
+    })
+
+})
